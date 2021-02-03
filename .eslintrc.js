1517573b54675fdb2c4cd9ef1ba5829795003a81
Noah Gibson <noah.gibson99@gmail.com>
2021-02-02 18:08:50 -0700

    Remove airbnb linting styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,11 +10,12 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:import/errors",
+    "plugin:react/recommended"
   ],
   globals: {
     process: true
   },
-  plugins: ["prettier"],
+  plugins: ["prettier", "react"],
   settings: {
     react: {
       version: "detect"
