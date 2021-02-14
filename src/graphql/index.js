2148f899c69d30ca6a7afed260b457017137d1ce
Noah Gibson <noah.gibson99@gmail.com>
2021-02-13 22:23:18 -0700

    Add resolvers to apollo export

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,6 +1,9 @@
 import { ApolloServer } from 'apollo-server-express'
+import * as resolvers from './resolvers'
 
 //Add config vars to apollo server
-const apolloServer = new ApolloServer({})
+const apolloServer = new ApolloServer({
+  resolvers: Object.values(resolvers),
+})
 
 export { apolloServer }
