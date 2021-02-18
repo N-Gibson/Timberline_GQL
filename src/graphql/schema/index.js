3857a977c0e0c474754d48ca334681de6231ac1c
Noah Gibson <noah.gibson99@gmail.com>
2021-02-17 17:03:53 -0700

    Update schema index

--- src/graphql/schema/index.js
+++ src/graphql/schema/index.js
@@ -1,1 +1,14 @@
-export * from './review'
+import { gql } from 'apollo-server-express'
+
+import reviewSchema from './review'
+
+const linkSchema = gql`
+  type Query {
+    _: Boolean
+  }
+  type Mutation {
+    _: Boolean
+  }
+`
+
+export default [linkSchema, reviewSchema]
