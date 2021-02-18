b2601c011a820dc6a7b113526672ad58b34378b8
Noah Gibson <noah.gibson99@gmail.com>
2021-02-17 17:03:31 -0700

    Update apollo server with type defs

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,8 +1,10 @@
 import { ApolloServer } from 'apollo-server-express'
 import * as resolvers from './resolvers'
+import typeDefs from './schema'
 
 //Add config vars to apollo server
 const apolloServer = new ApolloServer({
+  typeDefs,
   resolvers: Object.values(resolvers),
 })
 
